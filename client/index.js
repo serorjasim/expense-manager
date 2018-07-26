01f955175c937a18d6d45023427a864b4ebd92bf
seror <seror94ab@yahoo.com>
2018-07-26 00:20:27 -0500

    deploying

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'
+import Routes from './routes'
+
+ReactDOM.render(
+    <HashRouter>
+        <Routes />
+    </HashRouter>, document.getElementById('root')
+);
+
+
+
+
+
